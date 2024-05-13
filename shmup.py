40de0909d3f7b04450c70aaf44fd4ef287a340fb
newbieduck05 <136702382+newbieduck05@users.noreply.github.com>
2024-05-13 12:55:39 +0900

    Add files via upload

--- shmup.py
+++ shmup.py
@@ -1,6 +1,9 @@
 # Shmup game
 import pygame
 import random
+from os import path
+
+img_dir = path.join(path.dirname(__file__), 'img')
 
 WIDTH = 480
 HEIGHT = 600
@@ -12,6 +15,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
 # Initialize pygame and create window
 pygame.init()
@@ -23,9 +27,11 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(player_img, (50, 50))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        # self.radius = 20
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius, width=1)
         self.rect.centerx = WIDTH // 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -43,13 +49,20 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.x, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image = meteor_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        # self.radius = self.rect.width * 0,85 / 2
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius, width=1)
         self.rect.x = random.randrange(WIDTH - self.rect.width) # 480 - 30 = 450 --> 0 ~ 449
         self.rect.y = random.randrange(-100, -40)
         self.speedx = random.randrange(-3, 4)
@@ -64,9 +77,31 @@ class Mob(pygame.sprite.Sprite):
             self.speedx = random.randrange(-3, 4)
             self.sppedy = random.randrange(1, 8)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y) -> None:
+        super().__init__()        
+        self.image = bullet_img
+        self.image.set_colorkey (BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
+# Load all game graphics
+background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 
 player = Player()
 all_sprites.add(player)
@@ -86,16 +121,26 @@ while running:
         # Check for closing window
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     # Update
     all_sprites.update()
 
-    hits= pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    hits = pygame.sprite.spritecollide(player, mobs, False) # pygame.sprite.collide_circle
     if hits:
         running = False 
 
     # Draw / render
-    screen.fill(BLACK)
+    screen.blit(background, background_rect)
+    
     # Draw everything
     all_sprites.draw(screen)
 
